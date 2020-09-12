f5d757c4de84e5fa82949f36e52e08a2b2d05384
birdmaaan <eb.d@outlook.com>
2020-09-11 17:58:21 -0700

    finish background styling

--- dist/bundle.js
+++ dist/bundle.js
@@ -104,7 +104,8 @@ class Background {
   }
 
   draw() {
-    const scrollSpeed = 0.5;
+    const scrollSpeed = 0.25;
+    this.ctx.clearRect(0, 0, 1280, 720);
     this.ctx.drawImage(this.backgroundImage, 0, this.imgHeight, 1280, 720);
     this.ctx.drawImage(this.backgroundImage, 0, this.imgHeight - 720, 1280, 720);
     this.imgHeight += scrollSpeed;
